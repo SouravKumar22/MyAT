a00957cf550f82d8f6d95c996c0dcbd1081b3766
Sourav_Kumar <souravkumar91132@gmail.com>
2023-07-23 22:17:07 +0530

    Commit Message

--- app/src/main/java/com/example/myat/log_in.kt
+++ app/src/main/java/com/example/myat/log_in.kt
@@ -2,29 +2,82 @@ package com.example.myat
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 
 class log_in:AppCompatActivity() {
-
-    var login: Button? = null
-    var user: TextView? = null
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.log_in)
 
-        login = findViewById(R.id.lgn_btn)
-        user = findViewById(R.id.new_user_lgn)
+        auth = Firebase.auth
 
-        login!!.setOnClickListener{
-            val intent = Intent(this,MainActivity::class.java)
+        var log_in = findViewById<Button>(R.id.btnlogin)
+        var sgn_up = findViewById<TextView>(R.id.textViewSignUp)
+
+        log_in.setOnClickListener {
+            loginUser()
+
+
+        }
+
+        sgn_up.setOnClickListener {
+            var intent = Intent(this, sign_up::class.java)
             startActivity(intent)
         }
 
-        user!!.setOnClickListener{
-            val intent = Intent(this,sign_up::class.java)
-            startActivity(intent)
-        }
+    }
+
+    private fun loginUser() {
+        val auth = FirebaseAuth.getInstance()
+        val db = FirebaseFirestore.getInstance()
+        val empID = findViewById<EditText>(R.id.input_emp_id).text.toString()
+        val password = findViewById<EditText>(R.id.inputPassword).text.toString()
+        // Query Firestore to get the user's email associated with the employee ID
+        db.collection("users")
+            .whereEqualTo("emp_id", empID)
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+                if (!querySnapshot.isEmpty) {
+                    val userDocument = querySnapshot.documents[0]
+                    val userEmail = userDocument.getString("email")
+
+                    if (userEmail != null) {
+                        // Authenticate the user with the retrieved email and the password entered
+                        auth.signInWithEmailAndPassword(userEmail, password)
+                            .addOnCompleteListener(this) { task ->
+                                if (task.isSuccessful) {
+                                    // Login success, user is authenticated
+                                    Toast.makeText(this, "Login Successful!", Toast.LENGTH_LONG).show()
+                                    var intent = Intent(this, MainActivity::class.java)
+                                    startActivity(intent)
+                                    // Redirect to the home screen or perform other actions
+                                } else {
+                                    // Login failed, handle the error
+                                    Toast.makeText(this, "Login Failed: ${task.exception?.message}", Toast.LENGTH_LONG).show()
+                                }
+                            }
+                    } else {
+                        // Error: User email not found in Firestore
+                        Toast.makeText(this, "Employee ID not found!", Toast.LENGTH_LONG).show()
+                    }
+                } else {
+                    // Error: Employee ID not found in Firestore
+                    Toast.makeText(this, "Employee ID not found!", Toast.LENGTH_LONG).show()
+                }
+            }
+            .addOnFailureListener { e ->
+                // Error: Failed to query Firestore
+                Toast.makeText(this, "Failed to query Firestore: ${e.message}", Toast.LENGTH_LONG).show()
+            }
     }
 }
--- app/src/main/java/com/example/myat/sign_up.kt
+++ app/src/main/java/com/example/myat/sign_up.kt
@@ -1,17 +1,19 @@
 package com.example.myat
-
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 
 class sign_up: AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
+    var auth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,38 +21,87 @@ class sign_up: AppCompatActivity() {
 
         auth = Firebase.auth
 
-        var signUp = findViewById<Button>(R.id.sgn_up_btn)
-        var alreadyRegistered = findViewById<TextView>(R.id.already_user_sgn_up)
+        var signUp = findViewById<Button>(R.id.btnRegister)
+        var alreadyRegistered = findViewById<TextView>(R.id.alreadyHaveAccount)
 
 
-        signUp.setOnClickListener{
-            var intent = Intent(this,log_in::class.java)
+        signUp.setOnClickListener {
+            registerUser()
+
+        }
+        alreadyRegistered.setOnClickListener {
+            var intent = Intent(this, log_in::class.java)
             startActivity(intent)
         }
-        alreadyRegistered.setOnClickListener{
-            var intent = Intent(this,log_in::class.java)
-            startActivity(intent)
-        }
-
-        registerUser()
     }
 
-    private fun registerUser(){
-        val emp_n = findViewById<EditText>(R.id.et_emp_id_sgn_up)
-        val name = findViewById<EditText>(R.id.et_name_sgn_up)
-        val psswrd = findViewById<EditText>(R.id.et_pass_sgn_up)
-        val re_psswrd = findViewById<EditText>(R.id.et_pass_sgn_up1)
-        val mbl_num = findViewById<EditText>(R.id.et_phn_sgn_up)
-        val mail = findViewById<EditText>(R.id.et_mail_sgn_up)
-        val desg = findViewById<EditText>(R.id.et_desgntn_sgn_up)
+    private fun registerUser() {
+        val auth = FirebaseAuth.getInstance()
+        val db = FirebaseFirestore.getInstance()
 
-        val input_emp_id = emp_n.id
+
+        val emp_n = findViewById<EditText>(R.id.input_emp_id)
+        val name = findViewById<EditText>(R.id.inputname)
+        val psswrd = findViewById<EditText>(R.id.inputPassword)
+        val re_psswrd = findViewById<EditText>(R.id.inputConfirmPassword)
+        val mbl_num = findViewById<EditText>(R.id.input_number)
+        val mail = findViewById<EditText>(R.id.inputEmail)
+        val desg = findViewById<EditText>(R.id.input_designation)
+
+        val input_emp_id = emp_n.text.toString()
         val input_name = name.text.toString()
         val input_password = psswrd.text.toString()
         val input_re_password = re_psswrd.text.toString()
-        val mobile = mbl_num.id
+        val mobile = mbl_num.text.toString()
         val m = mail.text.toString()
         val designation = desg.text.toString()
-    }
 
+
+
+        if (input_password == input_re_password) {
+            auth.createUserWithEmailAndPassword(m, input_password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        //After Authentication adding user details to database
+                        val user = auth.currentUser
+                        if (user != null) {
+                            // User registration successful, save employee details to Firestore
+                            val userDetails = hashMapOf(
+                                "emp_id" to input_emp_id,
+                                "name" to input_name,
+                                "email" to m,
+                                "mobile" to mobile,
+                                "designation" to designation
+                            )
+                            db.collection("users").document(user.uid).set(userDetails)
+                                .addOnSuccessListener {
+                                    Toast.makeText(
+                                        this,
+                                        "User registered Successfully!",
+                                        Toast.LENGTH_LONG
+                                    ).show()
+                                    var intent = Intent(this, log_in::class.java)
+                                    startActivity(intent)
+                                }
+                                .addOnFailureListener { e ->
+                                    Toast.makeText(
+                                        this,
+                                        "Failed to store user details",
+                                        Toast.LENGTH_LONG
+                                    ).show()
+                                }
+                        }
+                    } else {
+                        Toast.makeText(
+                            this,
+                            "Failed to Register: ${task.exception?.message}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        } else {
+            Toast.makeText(this, "Passwords do not match", Toast.LENGTH_LONG).show()
+        }
+    }
 }
+
