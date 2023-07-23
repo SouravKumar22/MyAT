5ddb292349b4dd3ab722de88655c5e537c59ed92
Sourav_Kumar <souravkumar91132@gmail.com>
2023-07-23 23:06:30 +0530

    Commit Message

--- app/src/main/java/com/example/myat/MainActivity.kt
+++ app/src/main/java/com/example/myat/MainActivity.kt
@@ -1,11 +1,83 @@
 package com.example.myat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.example.myat.fragments.SettingFragment
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
+
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            onBackPressedMethod()
+        }
+
+    }
+
+    private fun onBackPressedMethod() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }else{
+            finish()
+        }
+    }
+
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        onBackPressedDispatcher.addCallback(this,onBackPressedCallback)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        drawerLayout = findViewById(R.id.drawerLayout)
+
+        val navigationView = findViewById<NavigationView>(R.id.navView)
+        val header = navigationView.getHeaderView(0)
+        val userNameTxt = findViewById<TextView>(R.id.usernameText)
+        val emailTxt = findViewById<TextView>(R.id.email)
+        val profileImg = findViewById<ImageView>(R.id.profileImg)
+
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(
+            this,drawerLayout,toolbar,R.string.open_drawer,R.string.close_drawer
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.nav_setting -> {
+                replaceFragment(SettingFragment())
+                title = "Setting"
+            }
+            R.id.nav_logout -> {
+                var intent = Intent(this, log_in::class.java)
+                Toast.makeText(this,"Logout", Toast.LENGTH_LONG).show()
+                startActivity(intent)
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragmentContainer,fragment)
+            .commit()
     }
 }
