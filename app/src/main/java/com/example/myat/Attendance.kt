46b5e2e6018f6e0ce787bc845f88168f620dcbf8
Sourav_Kumar <souravkumar91132@gmail.com>
2023-07-28 11:30:58 +0530

    Commit Message

--- app/src/main/java/com/example/myat/Attendance.kt
+++ app/src/main/java/com/example/myat/Attendance.kt
@@ -1,12 +1,14 @@
 package com.example.myat
 
 import android.app.DatePickerDialog
+import android.app.ProgressDialog.show
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
 import android.widget.Toast
+import com.google.android.play.integrity.internal.x
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -21,17 +23,8 @@ class Attendance : AppCompatActivity() {
         val rg = findViewById<RadioGroup>(R.id.radiogroup)
 
         dte = findViewById(R.id.tvDate)
-        val myCalendar = Calendar.getInstance()
-        val datePicker = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
-            myCalendar.set(Calendar.YEAR,year)
-            myCalendar.set(Calendar.MONTH,month)
-            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-            updateLable(myCalendar)
-        }
-
         dte.setOnClickListener {
-            DatePickerDialog(this,datePicker,myCalendar.get(Calendar.YEAR),
-            myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show()
+            showDatePicker()
         }
 
         rg.setOnCheckedChangeListener{ group, checkedID ->
@@ -50,10 +43,41 @@ class Attendance : AppCompatActivity() {
         }
     }
 
-    private fun updateLable(myCalendar: Calendar) {
-        val myFormat = "dd-MM-yyyy"
-        val sdf = SimpleDateFormat(myFormat, Locale.UK)
-        dte.setText(sdf.format(myCalendar.time))
-    }
 
+
+    private fun showDatePicker() {
+        val currentDate = Calendar.getInstance()
+        val year = currentDate.get(Calendar.YEAR)
+        val month = currentDate.get(Calendar.MONTH)
+        val day = currentDate.get(Calendar.DAY_OF_MONTH)
+
+        // Create the date picker dialog
+        val datePicker = DatePickerDialog(
+            this,
+            { _, selectedYear, selectedMonth, selectedDay ->
+                // Do something with the selected date if needed
+                // For example, you can display the selected date in a TextView
+                val selectedDate = Calendar.getInstance()
+                selectedDate.set(selectedYear, selectedMonth, selectedDay)
+                val formattedDate = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+                    .format(selectedDate.time)
+                dte.text = formattedDate
+            },
+            year,
+            month,
+            day
+        )
+
+        // Set the minimum and maximum selectable dates
+        val minDate = Calendar.getInstance()
+        minDate.add(Calendar.DAY_OF_MONTH, 0) // Disable past dates
+        datePicker.datePicker.minDate = minDate.timeInMillis
+
+        val maxDate = Calendar.getInstance()
+        maxDate.add(Calendar.MONTH,0) // Disable dates beyond 3 months in the future
+        datePicker.datePicker.maxDate = maxDate.timeInMillis
+
+        // Show the date picker dialog
+        datePicker.show()
+    }
 }
